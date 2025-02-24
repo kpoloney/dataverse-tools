e9b4d9ea198ec3acf377cebc8ee6c340db81a93b
Kelsey Poloney <poloke01@gmail.com>
2025-02-24 16:22:51 -0700

    Add file download count, license, title, subject fields

--- restricted.py
+++ restricted.py
@@ -3,7 +3,7 @@ import csv
 import requests
 
 # Script to get a list of datasets with restricted files and writes to a CSV file.
-# Includes number of restricted files, dataset DOI and publication date.
+# Includes number of restricted files, dataset DOI, publication date, license, and .
 
 api = "https://borealisdata.ca/api/"
 search = api + "search"
@@ -25,6 +25,12 @@ for dataset in search_metadata:
     md_single = export_single_md.json()
     metadata_all.append(md_single)
 
+# Get dowload counts (all files)
+dlapi = api+"info/metrics/filedownloads"
+dls_req = requests.get(dlapi,headers={'Accept':'application/json'},params={"parentAlias":"ualberta"})
+dls = dls_req.json()['data']
+
+# Identify datasets with restricted files
 restricted_datasets = []
 for ds in metadata_all:
     filelist = ds['datasetVersion']['files']
@@ -34,15 +40,42 @@ for ds in metadata_all:
             continue
         else:
             n_restr += 1
+            #Lookup file id in download count list - get total downloads of restricted files for this dataset
+            downloads = 0
+            file_id = file['dataFile']['id']
+            for item in dls:
+                if item['id'] == file_id:
+                    downloads += item['count']
+                    break
     if n_restr > 0:
-        info = {'doi':ds['persistentUrl'],
+        citation = ds['datasetVersion']['metadataBlocks']['citation']['fields']
+        # Get title
+        for field in citation:
+            if field['typeName'] == 'title':
+                title = field['value']
+            #Get subject
+            if field['typeName'] == 'subject':
+                subject = field['value']
+        # Get license
+        if 'license' in ds['datasetVersion'].keys():
+            license_name = ds['datasetVersion']['license']['name']
+        elif 'termsOfUse' in ds['datasetVersion'].keys():
+            if 'CC0 Waiver' in ds['datasetVersion']['termsOfUse']:
+                license_name = 'CC0 Waiver'
+            else:
+                license_name = 'Custom'
+        info = {'title':title,
+                'doi':ds['persistentUrl'],
                 'n_restricted_files':n_restr,
-                'date':ds['publicationDate']}
+                'date':ds['publicationDate'],
+                'license':license_name,
+                'downloads':downloads,
+                'subject':subject}
         restricted_datasets.append(info)
 
 #Write to csv
 with open(os.path.join("reports","restricted_files.csv"), 'w') as c:
-    writer = csv.DictWriter(c, fieldnames=['doi','n_restricted_files','date'])
+    writer = csv.DictWriter(c, fieldnames=list(info.keys()))
     writer.writeheader()
     for line in restricted_datasets:
         writer.writerow(line)
